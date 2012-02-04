aa6b59021fc0f3d04e8e41dc6e1ca9ea79d1f1d6
Helder Correia <helder.correia@palm.com>
2012-02-04 01:40:01 -0800

    Remove tabs in CmdResourceHandlers.h.

--- Src/CmdResourceHandlers.h
+++ Src/CmdResourceHandlers.h
@@ -23,66 +23,66 @@ LICENSE@@@ */
 
 class CommandHandler
 {
-	public:
-		CommandHandler( const std::string& cmd, const std::string& appId ) 
-				: m_command(cmd), m_appId(appId) { }
-		~CommandHandler() { }
-		
-		const std::string command() const { return m_command; }
-		const std::string appId() const { return m_appId; }
+    public:
+        CommandHandler( const std::string& cmd, const std::string& appId )
+            : m_command(cmd), m_appId(appId) { }
+        ~CommandHandler() { }
 
-	private:
-		std::string m_command;
-		std::string m_appId;
-		
-		CommandHandler();
+        const std::string command() const { return m_command; }
+        const std::string appId() const { return m_appId; }
+
+    private:
+        std::string m_command;
+        std::string m_appId;
+
+        CommandHandler();
 };
 
 class RedirectHandler
 {
-	public:
-		RedirectHandler(const std::string& scheme, const std::string& host, const std::string& appId )
-			: m_scheme(scheme) , m_host(host) , m_appId(appId) { }
-		virtual ~RedirectHandler() {}
-		
-		const std::string scheme() const { return m_scheme; }
-		const std::string host() const { return m_host; }
-		const std::string appId() const { return m_appId; }
-			
-	private:
-		
-		std::string m_scheme;
-		std::string m_host;
-		std::string m_appId;
-		
-		RedirectHandler();
+    public:
+        RedirectHandler(const std::string& scheme, const std::string& host, const std::string& appId )
+            : m_scheme(scheme) , m_host(host) , m_appId(appId) { }
+        virtual ~RedirectHandler() {}
+
+        const std::string scheme() const { return m_scheme; }
+        const std::string host() const { return m_host; }
+        const std::string appId() const { return m_appId; }
+
+    private:
+
+        std::string m_scheme;
+        std::string m_host;
+        std::string m_appId;
+
+        RedirectHandler();
 };
 
 class ResourceHandler
 {
-	public:
-		ResourceHandler( const std::string& ext, 
-				const std::string& contentType, 
-				const std::string& appId, 
-				bool stream=false ) 
-				: m_fileExt(ext)
-				, m_contentType(contentType)
-				, m_appId(appId)
-				, m_stream(stream) { }
-		~ResourceHandler() { }
-		
-		const std::string appId() const { return m_appId; }
-		const std::string fileExt() const { return m_fileExt; }
-		const std::string contentType() const { return m_contentType; }
-		bool stream() const { return m_stream; }
+    public:
+        ResourceHandler( const std::string& ext,
+                const std::string& contentType,
+                const std::string& appId,
+                bool stream=false )
+                : m_fileExt(ext)
+                , m_contentType(contentType)
+                , m_appId(appId)
+                , m_stream(stream) { }
+        ~ResourceHandler() { }
 
-	private:
-		std::string m_fileExt;
-		std::string m_contentType;
-		std::string m_appId;
-		bool m_stream;
-		
-		ResourceHandler();
+        const std::string appId() const { return m_appId; }
+        const std::string fileExt() const { return m_fileExt; }
+        const std::string contentType() const { return m_contentType; }
+        bool stream() const { return m_stream; }
+
+    private:
+        std::string m_fileExt;
+        std::string m_contentType;
+        std::string m_appId;
+        bool m_stream;
+
+        ResourceHandler();
 };
 
 
