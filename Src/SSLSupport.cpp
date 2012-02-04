612da4fc74ea9ca21ab706e4502e8188296a8efd
Helder Correia <helder.correia@palm.com>
2012-02-04 01:27:49 -0800

    Remove tabs in SSLSupport.cpp.

--- Src/SSLSupport.cpp
+++ Src/SSLSupport.cpp
@@ -31,66 +31,66 @@ long SSLSupport::s_nLocks = 0;
 
 unsigned long SSLSupport::getThreadID(void)
 {
-	return ((unsigned long) pthread_self());
+    return ((unsigned long) pthread_self());
 }
 
 void SSLSupport::lock(int lockingMode, int lockNumber, const char *,int)
 {
-	if (lockingMode & CRYPTO_LOCK) {
-		pthread_mutex_lock(&s_mutexArray[lockNumber]);
-	} else {
-		pthread_mutex_unlock(&s_mutexArray[lockNumber]);
-	}
+    if (lockingMode & CRYPTO_LOCK) {
+        pthread_mutex_lock(&s_mutexArray[lockNumber]);
+    } else {
+        pthread_mutex_unlock(&s_mutexArray[lockNumber]);
+    }
 }
 
 void SSLSupport::init(void)
 {
-	pthread_mutex_lock(&s_initLock);
-	if (s_initCounter) {
-		//already inited, exit
-		pthread_mutex_unlock(&s_initLock);
-		return;
-	}
-	++s_initCounter;
-	
-	SSL_load_error_strings();                /* readable error messages */
-	SSL_library_init();                      /* initialize library */
-	OpenSSL_add_all_ciphers();
-	OpenSSL_add_all_digests();
-	ERR_load_crypto_strings();
-	s_nLocks = CRYPTO_num_locks();
-	
-	s_mutexArray = (pthread_mutex_t*) OPENSSL_malloc(s_nLocks * sizeof(pthread_mutex_t));
+    pthread_mutex_lock(&s_initLock);
+    if (s_initCounter) {
+        //already inited, exit
+        pthread_mutex_unlock(&s_initLock);
+        return;
+    }
+    ++s_initCounter;
 
-	for (int i = 0; i < s_nLocks; i++) 
-	{
-		pthread_mutex_init(&s_mutexArray[i], NULL);
-	}
+    SSL_load_error_strings();                /* readable error messages */
+    SSL_library_init();                      /* initialize library */
+    OpenSSL_add_all_ciphers();
+    OpenSSL_add_all_digests();
+    ERR_load_crypto_strings();
+    s_nLocks = CRYPTO_num_locks();
 
-	CRYPTO_set_id_callback(getThreadID);
-	CRYPTO_set_locking_callback(lock);
-	
-	pthread_mutex_unlock(&s_initLock);
+    s_mutexArray = (pthread_mutex_t*) OPENSSL_malloc(s_nLocks * sizeof(pthread_mutex_t));
+
+    for (int i = 0; i < s_nLocks; i++) 
+    {
+        pthread_mutex_init(&s_mutexArray[i], NULL);
+    }
+
+    CRYPTO_set_id_callback(getThreadID);
+    CRYPTO_set_locking_callback(lock);
+
+    pthread_mutex_unlock(&s_initLock);
 }
 
 void SSLSupport::deinit(void)
 {
-	pthread_mutex_lock(&s_initLock);
-	
-	if (s_initCounter == 0) {
-		//already de-inited, exit
-		pthread_mutex_unlock(&s_initLock);
-		return;
-	}
-	--s_initCounter;
-	CRYPTO_set_locking_callback(NULL);
+    pthread_mutex_lock(&s_initLock);
 
-	for (int i = 0; i < s_nLocks; i++) {
-		pthread_mutex_destroy(&s_mutexArray[i]);
-	}
+    if (s_initCounter == 0) {
+        //already de-inited, exit
+        pthread_mutex_unlock(&s_initLock);
+        return;
+    }
+    --s_initCounter;
+    CRYPTO_set_locking_callback(NULL);
 
-	OPENSSL_free(s_mutexArray);
-	CRYPTO_set_id_callback(NULL);
-	
-	pthread_mutex_unlock(&s_initLock);
+    for (int i = 0; i < s_nLocks; i++) {
+        pthread_mutex_destroy(&s_mutexArray[i]);
+    }
+
+    OPENSSL_free(s_mutexArray);
+    CRYPTO_set_id_callback(NULL);
+
+    pthread_mutex_unlock(&s_initLock);
 }
