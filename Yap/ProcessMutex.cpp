1d32a2db59a3ee79447d47d516e0cd0f06f65369
Helder Correia <helder.correia@palm.com>
2012-02-04 17:36:48 -0800

    Remove tabs in ProcessMutex.cpp.

--- Yap/ProcessMutex.cpp
+++ Yap/ProcessMutex.cpp
@@ -32,122 +32,122 @@ LICENSE@@@ */
 static const uint32_t s_marker = 0xBAADF00D;
 
 struct Header {
-	uint32_t         marker1;
-	pthread_mutex_t  mutex;
-	uint32_t         marker2;
+    uint32_t         marker1;
+    pthread_mutex_t  mutex;
+    uint32_t         marker2;
 };
 
 ProcessMutex::ProcessMutex(int size, int key)
-	: m_key(key)
-	, m_data(0)
-	, m_dataSize(sizeof(Header) + size)
+    : m_key(key)
+    , m_data(0)
+    , m_dataSize(sizeof(Header) + size)
 {
-	if (key < 0) {
-	
-		while (m_key < 0) {
-			struct timeval tv;
-			gettimeofday(&tv, NULL);
-			key_t ipckey = ftok(".", tv.tv_usec);
-			m_key = ::shmget(ipckey, m_dataSize, 0644 | IPC_CREAT | IPC_EXCL);
-			if (m_key == -1 && errno != EEXIST) {
-				fprintf(stderr, "ProcessMutex: failed to create mutex: %s\n", strerror(errno));
-				return;
-			}
-		}
+    if (key < 0) {
 
-		m_data = ::shmat(m_key, NULL, 0);
-		if (-1 == (int)m_data) {
-			fprintf(stderr, "ERROR %d attaching to shared memory key %d: %s\n", errno, m_key, strerror(errno));
-			m_data = 0;
-			return;
-		}
-	
-		// Auto delete when all processes detach
-		::shmctl(m_key, IPC_RMID, NULL);
+        while (m_key < 0) {
+            struct timeval tv;
+            gettimeofday(&tv, NULL);
+            key_t ipckey = ftok(".", tv.tv_usec);
+            m_key = ::shmget(ipckey, m_dataSize, 0644 | IPC_CREAT | IPC_EXCL);
+            if (m_key == -1 && errno != EEXIST) {
+                fprintf(stderr, "ProcessMutex: failed to create mutex: %s\n", strerror(errno));
+                return;
+            }
+        }
 
-		Header* header = (Header*) m_data;
-		header->marker1 = s_marker;
-		header->marker2 = s_marker;
+        m_data = ::shmat(m_key, NULL, 0);
+        if (-1 == (int)m_data) {
+            fprintf(stderr, "ERROR %d attaching to shared memory key %d: %s\n", errno, m_key, strerror(errno));
+            m_data = 0;
+            return;
+        }
 
-		pthread_mutexattr_t attr;
-		pthread_mutexattr_init (&attr);
-		pthread_mutexattr_setpshared (&attr, PTHREAD_PROCESS_SHARED);
+        // Auto delete when all processes detach
+        ::shmctl(m_key, IPC_RMID, NULL);
 
-		pthread_mutex_t* mutex = (pthread_mutex_t*) &header->mutex;
-		pthread_mutex_init (mutex, &attr);
-		pthread_mutexattr_destroy(&attr);
-	}
-	else {
+        Header* header = (Header*) m_data;
+        header->marker1 = s_marker;
+        header->marker2 = s_marker;
 
-		m_data = ::shmat(m_key, NULL, 0);
-		if (m_data == (void*) -1) {
-			fprintf(stderr, "Failed to attach to shmid: %d, ERROR: %d, %s", m_key, errno, strerror(errno));
-			m_data = 0;
-			return;
-		}
+        pthread_mutexattr_t attr;
+        pthread_mutexattr_init (&attr);
+        pthread_mutexattr_setpshared (&attr, PTHREAD_PROCESS_SHARED);
 
-		if (!isValid()) {
-			fprintf(stderr, "Shared buffer is corrupted\n");
-			::shmdt(m_data);
-			m_data = 0;
-			return;
-		}			
-	}
+        pthread_mutex_t* mutex = (pthread_mutex_t*) &header->mutex;
+        pthread_mutex_init (mutex, &attr);
+        pthread_mutexattr_destroy(&attr);
+    }
+    else {
+
+        m_data = ::shmat(m_key, NULL, 0);
+        if (m_data == (void*) -1) {
+            fprintf(stderr, "Failed to attach to shmid: %d, ERROR: %d, %s", m_key, errno, strerror(errno));
+            m_data = 0;
+            return;
+        }
+
+        if (!isValid()) {
+            fprintf(stderr, "Shared buffer is corrupted\n");
+            ::shmdt(m_data);
+            m_data = 0;
+            return;
+
+    }
 }
 
 ProcessMutex::~ProcessMutex()
 {
-	if (m_data)
-		::shmdt(m_data);
+    if (m_data)
+        ::shmdt(m_data);
 }
 
 bool ProcessMutex::tryLock(int numTries)
 {
-	Header* header = (Header*) m_data;
+    Header* header = (Header*) m_data;
 
-	while (numTries > 0) {
-		if (pthread_mutex_trylock(&header->mutex) == 0)
-			return true;
+    while (numTries > 0) {
+        if (pthread_mutex_trylock(&header->mutex) == 0)
+            return true;
 
-		// FIXME: Will this really yield to a lower priority process?
-		(void)::sched_yield();
+        // FIXME: Will this really yield to a lower priority process?
+        (void)::sched_yield();
 
-		numTries--;
-	}
-	
-	return false;
+        numTries--;
+    }
+
+    return false;
 }
 
 void ProcessMutex::lock()
 {
-	Header* header = (Header*) m_data;
-	pthread_mutex_lock(&header->mutex);
+    Header* header = (Header*) m_data;
+    pthread_mutex_lock(&header->mutex);
 }
 
 void ProcessMutex::unlock()
 {
-	Header* header = (Header*) m_data;
+    Header* header = (Header*) m_data;
 
-	pthread_mutex_unlock(&header->mutex);
+    pthread_mutex_unlock(&header->mutex);
 }
 
 void* ProcessMutex::data() const
 {
     if (!m_data)
-		return 0;
+        return 0;
 
-	return ((uint8_t*) m_data + sizeof(Header));
+    return ((uint8_t*) m_data + sizeof(Header));
 }
 
 bool ProcessMutex::isValid() const
 {
-	if (!m_data)
-		return false;
+    if (!m_data)
+        return false;
 
-	Header* header = (Header*) m_data;
-	if (header->marker1 != s_marker ||
-		header->marker2 != s_marker)
-		return false;
+    Header* header = (Header*) m_data;
+    if (header->marker1 != s_marker ||
+        header->marker2 != s_marker)
+        return false;
 
-	return true;
+    return true;
 }
