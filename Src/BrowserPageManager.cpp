4e2070d389670ecaceec4edc3bcbcc33123470d4
Helder Correia <helder.correia@palm.com>
2012-02-06 15:04:35 -0800

    Fix possible inefficient checking for list emptiness.

--- Src/BrowserPageManager.cpp
+++ Src/BrowserPageManager.cpp
@@ -109,7 +109,7 @@ BrowserPageManager::unregisterPage(BrowserPage* page)
 void
 BrowserPageManager::watchForPage(BrowserPage* watchingPage, const int32_t identifier)
 {
-    if (m_watchingPageList.size() == 0) {  // attach page expire function to main loop
+    if (m_watchingPageList.empty()) {  // attach page expire function to main loop
         g_idle_add(expireWatchedPages, NULL);
     }
 
